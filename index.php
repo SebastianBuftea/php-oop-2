ce384a8289f427ba292fcf3f565bf5ff9e36841f
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-31 16:27:55 +0100

    [ADD] accesories, food and games classes

--- index.php
+++ index.php
@@ -16,4 +16,41 @@ class Product{
 
     }
 }
+
+class FoodPet extends Product{
+    public $grams;
+    public $ingredients;
+    public $dosage;
+
+     function __construct($_image, $_title, $_categoria, $_prezzo, $_type, $_grams, $_ingredients, $_dosage){
+       parent::__construct($_image, $_title, $_categoria, $_prezzo, $_type);
+       $this-> grams = $_grams;
+       $this-> ingredients = $_ingredients;
+       $this-> dosage = $_dosage; 
+    }
+}
+
+class AnimalGame extends Product{
+    public $material;
+    public $sound;
+
+     function __construct($_image, $_title, $_categoria, $_prezzo, $_type, $_material, $_sound){
+        parent::__construct($_image, $_title, $_categoria, $_prezzo, $_type);
+        $this-> material = $_material;
+        $this-> sound = $_sound; 
+     }
+}
+
+class Accesories extends Product{
+    public $description;
+    public $dimensions;
+
+    function __construct($_image, $_title, $_categoria, $_prezzo, $_type, $_description, $_dimension){
+        parent::__construct($_image, $_title, $_categoria, $_prezzo, $_type);
+        $this-> description = $_description;
+        $this-> dimension = $_dimension;
+    }
+
+}
+
 ?>
